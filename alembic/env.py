7ef76d68f06a95af5996d5d91bfb568d4df077ac
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2026-08-21 23:00:32 +0500

    [ALEMBIC] Role model migration

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from models import User, Event  # noqa
+from models import User, Event, Role  # noqa
 from database import Base
 from config import settings
 
