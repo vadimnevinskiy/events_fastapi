f3a162841eea2e3e297bac5bf8422f83205c829e
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2026-08-21 20:24:22 +0500

    [FEAT] Added relationship between User and Role

--- models/users.py
+++ models/users.py
@@ -1,10 +1,11 @@
 from typing import TYPE_CHECKING
 from database import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, ForeignKey
 
 if TYPE_CHECKING:
     from .events import Event
+    from .roles import Role
 
 
 class User(Base):
@@ -15,7 +16,11 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
 
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"), nullable=False)
+
+    role: Mapped["Role"] = relationship("Role")
+
     event_list: Mapped[list["Event"]] = relationship("Event", back_populates="author")
 
     def __repr__(self) -> str:
-        return f"<User id={self.id} name={self.name} email={self.email}>"
+        return f"<User id={self.id} name={self.name} email={self.email} role_id={self.role_id}>"
